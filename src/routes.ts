ea3863b6a39456c79440a7a54c4a89ca195f1de9
Andrea Bianchi <bianan96@gmail.com>
2022-05-07 00:46:22 +0200

    add routes boilerplates

--- src/routes.ts
+++ src/routes.ts
@@ -4,8 +4,26 @@ import { FastifyInstance } from "fastify";
 export const routes: FastifyPluginCallback = async (fastify, options) => {
     // Testing route
     fastify.get('/test', async (request, reply) => {
-        return { hello: 'world' };
+        return { hello: 'friends' };
     });
 
+    fastify.get('/blacklist', async (request, reply) => {
+        return { hello: 'friends' };
+    });
 
+    fastify.get('/id', async (request, reply) => {
+        return { hello: 'friends' };
+    });
+
+    fastify.post('/edit', async (request, reply) => {
+        return { hello: 'friends' };
+    });
+
+    fastify.post('/add', async (request, reply) => {
+        return { hello: 'friends' };
+    });
+
+    fastify.post('/del', async (request, reply) => {
+        return { hello: 'friends' };
+    });
 }
