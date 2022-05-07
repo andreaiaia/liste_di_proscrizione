e94df4e9bc4ea413f95b393734b31a0d1b1e5271
Andrea Bianchi <bianan96@gmail.com>
2022-05-07 22:39:43 +0200

    Add get all method, tried to connect to postgres db

--- App/routes.js
+++ App/routes.js
@@ -1,6 +1,56 @@
+import { addElement } from "./schemas.js";
+import pkg from "pg";
+const { Client } = pkg;
+const client = new Client({
+    user: `${process.env.POSTGRES_USER}`,
+    database: `${process.env.POSTGRES_DB}`,
+    password: `${process.env.POSTGRES_PASSWORD}`,
+    port: parseInt(process.env.POSTGRES_PORT || '5432')
+});
+client.connect();
 export const routes = async (fastify, options) => {
     // Testing route
-    fastify.get('/test', async (request, reply) => {
-        return { hello: 'world' };
+    // fastify.get('/test', async (req, reply) => {
+    //     return { hello: 'friends' };
+    // });
+    fastify.get('/blacklist', async (req, reply) => {
+        try {
+            const result = client.query("SELECT * FROM blacklisted");
+            reply.status(200).send(result);
+        }
+        catch (err) {
+            throw err;
+        }
+    });
+    fastify.get('/id', async (req, reply) => {
+        return { hello: 'friends' };
+    });
+    fastify.post('/edit', { schema: { body: addElement } }, async (req, reply) => {
+        return { hello: 'friends' };
+        // const { first_name, last_name, email, phone, blacklisted, last_edit } = req.body;
+        // const query = {
+        //     text: `INSERT INTO blacklisted (
+        //                 first_name, 
+        //                 last_name, 
+        //                 email, 
+        //                 phone, 
+        //                 blacklisted, 
+        //                 last_edit
+        //             )
+        //             VALUES($1, $2, $3, $4, $5, $6 ) RETURNING *`,
+        //     values: [first_name, last_name, email, phone, blacklisted, last_edit],
+        // };
+        // try {
+        //     reply.code(201);
+        //     return { created: true };
+        // } catch (err) {
+        //     throw new Error(err);
+        // }
+    });
+    fastify.post('/add', async (req, reply) => {
+        return { hello: 'friends' };
+    });
+    fastify.post('/del', async (req, reply) => {
+        return { hello: 'friends' };
     });
 };
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,17 @@
 import { FastifyPluginCallback } from "fastify";
-import { addElement } from "./schemas";
+import { addElement } from "./schemas.js";
+import pkg from "pg";
+
+const { Client } = pkg;
+
+const client = new Client({
+    user: `${process.env.POSTGRES_USER}`,
+    database: `${process.env.POSTGRES_DB}`,
+    password: `${process.env.POSTGRES_PASSWORD}`,
+    port: parseInt(process.env.POSTGRES_PORT || '5432')
+});
+
+client.connect();
 
 export const routes: FastifyPluginCallback = async (fastify, options) => {
     // Testing route
@@ -8,7 +20,12 @@ export const routes: FastifyPluginCallback = async (fastify, options) => {
     });
 
     fastify.get('/blacklist', async (req, reply) => {
-        return { hello: 'friends' };
+        try {
+            const result = client.query("SELECT * FROM blacklisted");
+            reply.status(200).send(result);
+        } catch (err) {
+            throw err;
+        }
     });
 
     fastify.get('/id', async (req, reply) => {
@@ -16,27 +33,28 @@ export const routes: FastifyPluginCallback = async (fastify, options) => {
     });
 
     fastify.post('/edit', { schema: { body: addElement } }, async (req, reply) => {
-        const { first_name, last_name, email, phone, blacklisted, last_edit } = req.body;
+        return { hello: 'friends' };
+        // const { first_name, last_name, email, phone, blacklisted, last_edit } = req.body;
 
-        const query = {
-            text: `INSERT INTO blacklisted (
-                        first_name, 
-                        last_name, 
-                        email, 
-                        phone, 
-                        blacklisted, 
-                        last_edit
-                    )
-                    VALUES($1, $2, $3, $4, $5, $6 ) RETURNING *`,
-            values: [first_name, last_name, email, phone, blacklisted, last_edit],
-        };
+        // const query = {
+        //     text: `INSERT INTO blacklisted (
+        //                 first_name, 
+        //                 last_name, 
+        //                 email, 
+        //                 phone, 
+        //                 blacklisted, 
+        //                 last_edit
+        //             )
+        //             VALUES($1, $2, $3, $4, $5, $6 ) RETURNING *`,
+        //     values: [first_name, last_name, email, phone, blacklisted, last_edit],
+        // };
 
-        try {
-            reply.code(201);
-            return { created: true };
-        } catch (err) {
-            throw new Error(err);
-        }
+        // try {
+        //     reply.code(201);
+        //     return { created: true };
+        // } catch (err) {
+        //     throw new Error(err);
+        // }
     });
 
     fastify.post('/add', async (req, reply) => {
