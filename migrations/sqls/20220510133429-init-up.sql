776d60c7993c9521d00962d0930ad4b028205429
Andrea Bianchi <bianan96@gmail.com>
2022-05-10 15:50:27 +0200

    start adding migrations

---
+++ migrations/sqls/20220510133429-init-up.sql
@@ -0,0 +1,16 @@
+/* Create a new empty table */
+/* you can launch this migration with the command */
+/* db-migrate up init */
+
+CREATE TABLE blacklist 
+(
+    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    is_blocked BOOLEAN NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    last_edit TIMESTAMP NOT NULL DEFAULT NOW(),
+    last_edited_by VARCHAR(100) NOT NULL
+);
